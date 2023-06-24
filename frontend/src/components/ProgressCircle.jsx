7c7234e3c27f14c841b351a1b84c9170d6b4b18c
sharmaya_1 <112720965+Yashh-777@users.noreply.github.com>
2023-06-24 11:37:33 +0530

    Update ProgressCircle.jsx

--- frontend/src/components/ProgressCircle.jsx
+++ frontend/src/components/ProgressCircle.jsx
@@ -1,6 +1,7 @@
 import {Box, useTheme} from '@mui/material';
 import {tokens } from "../theme";
 
+
 const ProgressCircle = ({progress = "0.75", size = "40"}) => {
     const theme = useTheme();
     const colors = tokens(theme.palette.mode);
