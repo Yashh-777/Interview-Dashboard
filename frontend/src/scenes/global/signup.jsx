a786fe3d3639ae79de2f9b5f6aeb59689e3a1a66
sharmaya_1 <112720965+Yashh-777@users.noreply.github.com>
2023-06-29 13:20:12 +0530

    Update signup.jsx

--- frontend/src/scenes/global/signup.jsx
+++ frontend/src/scenes/global/signup.jsx
@@ -11,7 +11,7 @@ import styled from "styled-components";
 
 const SignUp = () => {
   const isNonMobile = useMediaQuery("(min-width:600px)");
-
+  
   const handleFormSubmit = (values) => {
     console.log(values);
   };
